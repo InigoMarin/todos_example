6a302e707458b755772fccbdf87daf84ba0f5f9c
Iñigo Marin <inigomaringarcia@gmail.com>
2020-09-05 18:31:35 +0200

    Node mode connection ok

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
 const port = process.env.PORT || 8000;
+
+const mongo_uri = 'mongodb://root:example@localhost:27017/todos?authSource=admin';
+
+mongoose.connect(mongo_uri,{
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 app.get("/",(req,res)=> res.status(200).send("Hello World"));
 
