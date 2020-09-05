8b8859f263661e8dc54dba9d79b37ba093872c26
Iñigo Marin <inigomaringarcia@gmail.com>
2020-09-05 17:46:04 +0200

    Express hello world

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
 import express from 'express';
 
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.get("/",(req,res)=> res.status(200).send("Hello World"));
+
+app.listen(port,()=> console.log(`Listening on localhost:${port}`));
+
 
